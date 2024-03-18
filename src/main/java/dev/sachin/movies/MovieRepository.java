38c79a91cfc88fd68f488b8a007ab8f2284f4546
sachinpatel9135 <sachin.patel.abcdef@gmail.com>
2024-03-18 19:53:17 +0530

    all changes

--- src/main/java/dev/sachin/movies/MovieRepository.java
+++ src/main/java/dev/sachin/movies/MovieRepository.java
@@ -1,2 +1,12 @@
-package dev.sachin.movies;public interface MovieRepository {
+package dev.sachin.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    public Optional<Movie> findMovieByImdbId(String imdbId);
 }
--- src/main/java/dev/sachin/movies/ReviewRepository.java
+++ src/main/java/dev/sachin/movies/ReviewRepository.java
@@ -1,2 +1,9 @@
-package dev.sachin.movies;public interface ReviewRepository {
+package dev.sachin.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository 
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
